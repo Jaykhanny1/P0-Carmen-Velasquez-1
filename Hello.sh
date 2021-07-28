f4b77186d3fa48ad1e0c882c4e56afed26cb6c89
Velcar19 <vcar352965@gmail.com>
2021-07-28 15:50:30 -0500

    scripts

---
+++ Hello.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+echo "What are your top 3 favorite movies?" 
+read -a movies
+echo "So, your favorite movies are: ${movies[0]}, ${movies[1]}, and ${movies[2]}?"
+
+#The Conjuring
+#Annabelle: Creation 
+#Haunted House 
